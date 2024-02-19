aa28c19c726b59faf7a9f2c507fdd4ad2a82965e
Hadrien Hamel <hadrien.hamel@gmail.com>
2024-02-19 22:05:10 +0100

    Added basic error handling when pushing image to prusaconnect

--- pi-prusacam.py
+++ pi-prusacam.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python3
 from prusacamconfig import PRUSACONNECT_URL, FINGERPRINT, CAMERA_TOKEN, PRUSACONNECT_TIMEOUT_S
 import requests
-
-def push_to_prusaconnect(jpg_image):
-    response = requests.put(
-        PRUSACONNECT_URL,
-        headers={
-            "accept": "application/json",
-            "content-type": "image/jpg",
-            "fingerprint": FINGERPRINT,
-            "token": CAMERA_TOKEN,
-        },
-        data=jpg_image,
-        timeout=PRUSACONNECT_TIMEOUT_S
-    )
-    if response.status_code != 204:
-        print(response.content)
-    else:
-        print("Success")
-
+from requests.exceptions import ConnectionError, Timeout
 import os
+
+
+def push_to_prusaconnect(jpg_image, timeout_s=PRUSACONNECT_TIMEOUT_S):
+    try:
+        response = requests.put(
+            PRUSACONNECT_URL,
+            headers={
+                "accept": "application/json",
+                "content-type": "image/jpg",
+                "fingerprint": FINGERPRINT,
+                "token": CAMERA_TOKEN,
+            },
+            data=jpg_image,
+            timeout=timeout_s
+        )
+        if response.status_code != 204:
+            print(response.content)
+        else:
+            print("Success")
+    except ConnectionError as conn_err:
+        print(f'Connection error occurred: {conn_err}')
+    except Timeout as _:
+        # The request timed out
+        print(f'Timeout error: The request took longer than the timeout value of {timeout_s} seconds.')
+    except Exception as e:
+        # Handle other possible exceptions
+        print(f'An unexpected error occurred: {e}')
+
+
 if os.uname().sysname == 'Darwin':
     # MacOS
     import imageio as iio
+
     # import matplotlib.pyplot as plt
 
     camera = iio.get_reader("<video0>")
